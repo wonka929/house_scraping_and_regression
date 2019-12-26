6396612391708a06dbbda7fdc140bf6d1f72659d
unknown <wonka929@gmail.com>
2019-12-26 16:57:39 +0100

    Version_RC1

--- tpot_torino_pipeline.py
+++ tpot_torino_pipeline.py
@@ -1,14 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.cluster import FeatureAgglomeration
-from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.linear_model import LassoLarsCV
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import PolynomialFeatures, RobustScaler
-from tpot.builtins import StackingEstimator, ZeroCount
-from sklearn.preprocessing import FunctionTransformer
-from copy import copy
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -16,19 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:-17930.474300675214
+# Average CV score on the training set was:-11502.534274945594
 exported_pipeline = make_pipeline(
-    make_union(
-        make_pipeline(
-            FeatureAgglomeration(affinity="euclidean", linkage="ward"),
-            RobustScaler()
-        ),
-        FunctionTransformer(copy)
-    ),
-    ZeroCount(),
-    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
-    StackingEstimator(estimator=LassoLarsCV(normalize=False)),
-    ExtraTreesRegressor(bootstrap=False, max_features=0.8500000000000001, min_samples_leaf=11, min_samples_split=16, n_estimators=100)
+    MinMaxScaler(),
+    GradientBoostingRegressor(alpha=0.95, learning_rate=0.1, loss="ls", max_depth=8, max_features=0.2, min_samples_leaf=2, min_samples_split=16, n_estimators=100, subsample=0.9500000000000001)
 )
 
 exported_pipeline.fit(training_features, training_target)
