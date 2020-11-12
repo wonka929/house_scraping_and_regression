f9985af2a0c8891a7d1466d8ba2dabdf3212bd2d
unknown <wonka929@gmail.com>
2020-11-12 19:58:35 +0100

    Final Jupyter Notebooks for updating Medium Article

--- Scrape_immobiliare.py
+++ Scrape_immobiliare.py
@@ -7,8 +7,11 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from tqdm import tqdm as tqdm
+#from tqdm import tqdm_notebook as tqdm
+from tqdm.notebook import tqdm as tqdm
 import csv
+from joblib import Parallel, delayed
+import multiprocessing
 
 
 # In[2]:
@@ -38,7 +41,7 @@ def connect(web_addr):
 def get_areas(website):
     data = connect(website)
     areas = []
-    for ultag in data.find_all('ul', {'class': 'breadcrumb-list breadcrumb-list_list thebigonelist--mouse'}):
+    for ultag in data.find_all('ul', {'class': 'breadcrumb-list breadcrumb-list_list breadcrumb-list__related'}):
         for litag in ultag.find_all('li'):
             for i in range(len(litag.text.split(','))):
                 areas.append(litag.text.split(',')[i])
@@ -49,7 +52,7 @@ def get_areas(website):
         url = website + '/' + area.replace(' ','-').lower()
         urls.append(url)
     
-    return urls
+    return areas, urls
 
 def get_apartment_links(website):
     data = connect(website)
@@ -63,54 +66,98 @@ def get_apartment_links(website):
                 continue
     return links
 
+#def scrape_link(website):
+#    data = connect(website)
+#    nomi = []
+#    valori = []
+#    temp = []
+#    for link in (data.find_all('dd', {'class': 'im-features__value'})[1], data.find_all('dd', {'class': 'im-features__value'})[9], data.find_all('dd', {'class': 'im-features__value'})[12]):
+#        temp.append(link)
+#        try:
+#            temp = str(temp).replace('</dd>', '')
+#            temp = str(temp).replace('<dt', '</dd><dt')
+#            temp = BeautifulSoup(temp, "html.parser")
+#            for elem in temp.find_all('dd'):
+#                valori.append(elem.string.strip())
+#        except:
+#            #print('errore')
+#            pass
+#        temp = []
+#
+#    valori[-1] = (data.find_all('dd', {'class': 'im-features__value'})[0].find_all('span')[0].string)
+#
+#    for link in data.find_all('dt'):
+#        if link.string == None:
+#            pass
+#        else:
+#            nomi.append(link.string.strip())
+#
+#    while len(valori)<len(nomi):
+#        valori.append(0)
+#
+#    while len(nomi)<len(valori):
+#        nomi.append('Prestazione energetica del fabbricato')
+#
+#    count = 0
+#    nomi.append('Arredato S/N')
+#    nomi.remove('altre caratteristiche')
+#    for elem in data.find_all('dd', {'class': 'im-features__value'})[8].find_all('span'):
+#        if elem.string.strip() == "Arredato" or elem.string == "Non arredato" or elem.string == "Parzialmente arredato":
+#            valori.append(elem.string.strip())
+#            count = 1            
+#        else:
+#            continue
+#    if count == 0:
+#        valori.append('0')        
+#
+#    valori = remove_duplicates(valori)
+#    nomi = remove_duplicates(nomi)
+#    
+#    return nomi, valori
+
 def scrape_link(website):
     data = connect(website)
-    nomi = []
-    valori = []
-    for link in data.find_all('dd', {'class': 'col-xs-12'}):
+    info = data.find_all('dl', {'class': 'im-features__list'})
+    comp_info = pd.DataFrame()
+    cleaned_id_text = []
+    cleaned_id__attrb_text = []
+    for n in range(len(info)):
+        for i in info[n].find_all('dt'):
+            cleaned_id_text.append(i.text)
+        for i in info[n].find_all('dd'):
+            cleaned_id__attrb_text.append(i.text)
+
+    comp_info['Id'] = cleaned_id_text
+    comp_info['Attribute'] = cleaned_id__attrb_text
+    comp_info
+    feature = []
+    for item in comp_info['Attribute']:
         try:
-            valore_affitto = link.find("span")
-            if valore_affitto.string != None:
-                valori.append(valore_affitto.string)
+            feature.append(clear_df(item))
         except:
-            pass
-        
-        if link.string == None:
-            pass
-        else:
-            valori.append(link.string)
+            feature.append(ultra_clear_df(item))
+
+    comp_info['Attribute'] = feature
+    #comp_info.set_index('Id', drop=True, inplace=True)
+    #comp_info.index.name = None
+    #comp_info = comp_info.T.reset_index().drop(columns='index')
+    return comp_info['Id'].values, comp_info['Attribute'].values
     
-        
-    for link in data.find_all('dt'):
-        if link.string == None:
-            pass
-        else:
-            nomi.append(link.string)
-    
-    valori = remove_duplicates(valori)
-    nomi = remove_duplicates(nomi)
-    
-    while len(valori)<len(nomi):
-        valori.append(0)
-        
-    while len(nomi)<len(valori):
-        nomi.append('Prestazione energetica del fabbricato')
-    
-    count = 0
-    nomi.append('Arredato S/N')
-    for elem in data.find_all('span', {'class': 'label label-gray'}):
-        if elem.string == "Arredato" or elem.string == "Non arredato" or elem.string == "Parzialmente arredato":
-            valori.append(elem.string)
-            count = 1            
-        else:
-            continue
-    if count == 0:
-        valori.append('0')
-    
-    return nomi, valori
 
 def remove_duplicates(x):
     return list(dict.fromkeys(x))
+
+
+def clear_df(the_list):
+    the_list = (the_list.split('\n')[1].split('  '))
+    the_list = [value for value in the_list if value != ''][0]
+    return the_list
+
+def ultra_clear_df(the_list):
+    the_list = (the_list.split('\n\n')[1].split('  '))
+    the_list = [value for value in the_list if value != ''][0]
+    the_list = (the_list.split('\n')[0])
+    return the_list
 
 
 # In[3]:
@@ -120,7 +167,7 @@ def remove_duplicates(x):
 ## get areas inside the city (districts)
 
 website = "https://www.immobiliare.it/affitto-case/torino"
-districts = get_areas(website)
+areas, districts = get_areas(website)
 print("Those are district's links \n")
 print(districts)
 
@@ -163,26 +210,15 @@ with open('announces_list.csv', 'w') as myfile:
 
 ## DATAFRAME ORIZZONTALE__USARE
 
-variabili = ['Riferimento e Data annuncio', 'Contratto', 'Tipologia', 'Superficie', 'Locali', 'Piano', 'Disponibilità', 'Tipo proprietà', 'Prezzo', 'Spese condominio', 'Spese riscaldamento', 'Informazioni catastali', 'Anno di costruzione', 'Stato', 'Riscaldamento', 'Climatizzatore', 'Classe energetica', 'Prestazione energetica del fabbricato', 'Arredato S/N']
-df_scrape = pd.DataFrame(columns=variabili)
-#df_scrape.set_index(0, inplace=True, drop=True)
-#print(df_scrape)
+df_scrape = pd.DataFrame()
 to_be_dropped = []
 counter = 0
 for link in tqdm(list(announces_links)):
-    #print(link)
     counter=counter+1
     try:
         nomi, valori = scrape_link(link)
         df_temporaneo = pd.DataFrame(columns=nomi)
-        #df_temporaneo['Nomi'] = nomi
-        #df_temporaneo.set_index('Nomi',inplace=True,drop=True)
-        #print(len(nomi),len(valori))
-        #while len(valori)<len(nomi):
-        #    valori.append(0)
-        #print(len(valori[0:len(nomi)]))
         df_temporaneo.loc[len(df_temporaneo), :] = valori[0:len(nomi)]
-        #print(df_temporaneo)
         df_scrape = df_scrape.append(df_temporaneo, sort=False)
     except Exception as e:
         print(e)
@@ -208,44 +244,40 @@ to_be_dropped.sort(reverse=True)
 # In[9]:
 
 
-to_be_dropped
+for index in to_be_dropped:
+    del location[index-1]
 
 
 # In[10]:
 
 
 for index in to_be_dropped:
-    del location[index-1]
+    del announces_links[index-1]
 
 
 # In[11]:
 
 
-for index in to_be_dropped:
-    del announces_links[index-1]
+print(df_scrape.shape)
+df_scrape['zona'] = location
+df_scrape['links'] = announces_links
+df_scrape.columns = map(str.lower, df_scrape.columns)
+df_scrape.to_csv('dataset.csv', sep=";")
 
 
 # In[12]:
 
 
-len(location)
+df_scrape = pd.read_csv('dataset.csv', sep=';')
 
 
 # In[13]:
 
 
-print(df_scrape.shape)
-df_scrape['Zona'] = location
-df_scrape.to_csv('dataset.csv', sep=";")
+df_scrape = df_scrape[['contratto', 'zona', 'tipologia', 'superficie', 'locali', 'piano', 'tipo proprietà', 'prezzo', 'spese condominio', 'spese aggiuntive', 'anno di costruzione', 'stato', 'riscaldamento', 'climatizzazione', 'classe energetica', 'posti auto', 'links']]
 
 
 # In[14]:
-
-
-df_scrape = df_scrape[['Contratto', 'Zona', 'Tipologia', 'Superficie', 'Locali', 'Piano', 'Tipo proprietà', 'Prezzo', 'Spese condominio', 'Spese riscaldamento','Anno di costruzione', 'Stato', 'Riscaldamento', 'Climatizzatore', 'Classe energetica', 'Arredato S/N']]
-
-
-# In[23]:
 
 
 def cleanup(df):
@@ -267,13 +299,13 @@ def cleanup(df):
     pr_type = []
     arredato = []
     
-    for tipo in df['Tipologia']:
+    for tipo in df['tipologia']:
         try:
             tipologia.append(tipo)
         except:
             tipologia.append(None)
     
-    for superficie in df['Superficie']:
+    for superficie in df['superficie']:
         try:
             if "m" in superficie:
                 s = superficie.replace(" m²", "")
@@ -281,37 +313,37 @@ def cleanup(df):
         except:
             surface.append(None)
     
-    for locali in df['Locali']:
+    for locali in df['locali']:
         try:
             rooms.append(locali[0:1])
         except:
             rooms.append(None)
     
-    for prezzo in df['Prezzo']:
+    for prezzo in df['prezzo']:
         try:
-            price.append(prezzo.replace("Affitto ", "").replace("€ ", "").replace(" al mese", "").replace(".",""))
+            price.append(prezzo.replace("Affitto ", "").replace("€ ", "").replace("/mese", "").replace(".",""))
         except:
             price.append(None)
             
-    for contratto in df['Contratto']:
+    for contratto in df['contratto']:
         try:
             contract.append(contratto.replace("\n ",""))
         except:
             contract.append(None)
     
-    for piano in df['Piano']:
+    for piano in df['piano']:
         try:
             floor.append(piano.split(' ')[0])
         except:
             floor.append(None)
     
-    for tipologia in df['Tipo proprietà']:
+    for tipologia in df['tipo proprietà']:
         try:
             pr_type.append(tipologia.split(',')[0])
         except:
             pr_type.append(None)
             
-    for condo in df['Spese condominio']:
+    for condo in df['spese condominio']:
         try:
             if "mese" in condo:
                 condominio.append(condo.replace("€ ","").replace("/mese",""))
@@ -320,7 +352,7 @@ def cleanup(df):
         except:
             condominio.append(None)
         
-    for ii in df['Spese riscaldamento']:
+    for ii in df['spese aggiuntive']:
         try:
             if "anno" in ii:
                 mese = int(int(ii.replace("€ ","").replace("/anno","").replace(".",""))/12)
@@ -329,68 +361,71 @@ def cleanup(df):
                 heating.append(None)
         except:
             heating.append(None)
-    
-    for anno_costruzione in df['Anno di costruzione']:
+   
+    for anno_costruzione in df['anno di costruzione']:
         try:
             built_in.append(anno_costruzione)
         except:
             built_in.append(None)
     
-    for stato in df['Stato']:
+    for stato in df['stato']:
         try:
             stat = stato.replace(" ","").lower()
             state.append(stat)
         except:
             state.append(None)
     
-    for tipo_riscaldamento in df['Riscaldamento']:
+    for tipo_riscaldamento in df['riscaldamento']:
         try:
-            riscaldamento.append(tipo_riscaldamento.lower().split(',')[0])
+            if 'Centralizzato' in tipo_riscaldamento:
+                riscaldamento.append('centralizzato')
+            elif 'Autonomo' in tipo_riscaldamento:
+                riscaldamento.append('autonomo')
         except:
             riscaldamento.append(None)
     
-    for clima in df['Climatizzatore']:
+    for clima in df['climatizzazione']:
         try:
             cooling.append(clima.lower().split(',')[0])
         except:
             cooling.append('None')
     
-    for classe in df['Classe energetica']:
+    for classe in df['classe energetica']:
         try:
-            energy_class.append(classe.replace("\n ",""))
+            energy_class.append(classe[0:2])#.replace("\n ",""))
         except:
             energy_class.append(None)
             
-    for SN in df['Arredato S/N']:
-        try:
-            arredato.append(SN)
-        except:
-            arredato.append(None)
-            
+    #for SN in df['Arredato S/N']:
+    #    try:
+    #        arredato.append(SN)
+    #    except:
+    #        arredato.append(None)
+    #        
     
-    final_df = pd.DataFrame(columns=['Contratto', 'Zona', 'Tipologia', 'Superficie', 'Locali', 'Piano', 'Tipo proprietà', 'Prezzo', 'Spese condominio', 'Spese riscaldamento','Anno di costruzione', 'Stato', 'Riscaldamento', 'Climatizzatore', 'Classe energetica', 'Arredato S/N'])
-    final_df['Contratto'] = contract
-    final_df['Tipologia'] = tipologia
-    final_df['Superficie'] = surface
-    final_df['Locali'] = rooms
-    final_df['Piano'] = floor
-    final_df['Tipo proprietà'] = pr_type
-    final_df['Prezzo'] = price
-    final_df['Spese condominio'] = condominio
-    final_df['Spese riscaldamento'] = heating
-    final_df['Anno di costruzione'] = built_in
-    final_df['Stato'] = state
-    final_df['Riscaldamento'] = riscaldamento
-    final_df['Climatizzatore'] = cooling
-    final_df['Classe energetica'] = energy_class
-    final_df['Zona'] = df['Zona'].values
-    final_df['Arredato S/N'] = arredato
-    final_df['Link annuncio'] = announces_links
+    final_df = pd.DataFrame(columns=['contratto', 'zona', 'tipologia', 'superficie', 'locali', 'piano', 'tipo proprietà', 'prezzo', 'spese condominio', 'spese aggiuntive', 'anno di costruzione', 'stato', 'riscaldamento', 'climatizzazione', 'certificazione energetica', 'posti auto'])#, 'Arredato S/N'])
+    final_df['contratto'] = contract
+    final_df['tipologia'] = tipologia
+    final_df['superficie'] = surface
+    final_df['locali'] = rooms
+    final_df['piano'] = floor
+    final_df['tipo proprietà'] = pr_type
+    final_df['prezzo'] = price
+    final_df['spese condominio'] = condominio
+    final_df['spese riscaldamento'] = heating
+    final_df['anno di costruzione'] = built_in
+    final_df['stato'] = state
+    final_df['riscaldamento'] = riscaldamento
+    final_df['climatizzatore'] = cooling
+    final_df['classe energetica'] = energy_class
+    final_df['zona'] = df['zona'].values
+    #inal_df['Arredato S/N'] = arredato
+    final_df['link annuncio'] = announces_links
     
     return final_df
 
 
-# In[24]:
+# In[15]:
 
 
 final = cleanup(df_scrape)
